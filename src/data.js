c224c9cfbaf24441b704f985c91240eafba6700b
Charly4444 <Charleydynamic@gmail.com>
2023-03-02 18:50:26 +0100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const peopleList = [
+    {
+        id: 1,
+        name: 'Bertie Yates',
+        age: 0,
+        dob: '1994-02-02',
+        birthday: false
+    },
+    {
+        id: 2,
+        name: 'Hester Hogan',
+        age: 0,
+        dob: '1991-02-01',
+        birthday: false
+    },
+    {
+        id: 3,
+        name: 'Larry Little',
+        age: 0,
+        dob: '1987-03-02',
+        birthday: false
+    },
+    {
+        id: 4,
+        name: 'Sean Walsh',
+        age: 0,
+        dob: '1989-02-03',
+        birthday: false
+    },
+    {
+        id: 5,
+        name: 'Lola Gardner',
+        age: 0,
+        dob: '1994-03-08',
+        birthday: false
+    }
+];
+
+export default peopleList;
